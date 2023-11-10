7f475dcc009dd91bb33bea8686a585b558c31787
boldfaced7 <boldfaced753@gmail.com>
2023-11-11 00:44:26 +0900

    #6

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker load -i /home/ec2-user/app/one-day-a-pill-BE.tar
-docker run -d -p 8080:8080 one-day-a-pill-BE
+docker load -i /home/ec2-user/app/one-day-a-pill-be.tar
+docker run -d -p 8080:8080 one-day-a-pill-be
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-if docker ps | grep -q 'your-docker-image'; then
+if docker ps | grep -q one-day-a-pill-be; then
   docker stop $(docker ps -q --filter ancestor=your-docker-image)
 fi
