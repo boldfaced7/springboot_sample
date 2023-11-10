24851ebfb6f029e56f7998b37d80eb74c14beb03
boldfaced7 <boldfaced753@gmail.com>
2023-11-11 02:25:01 +0900

    #16

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-docker load -i /home/ec2-user/app/one-day-a-pill-be.tar
-docker run -d -p 8080:8080 one-day-a-pill-be
+docker run -d -p 80:8080 one-day-a-pill-be
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 if docker ps | grep -q one-day-a-pill-be; then
-  docker stop $(docker ps -q --filter ancestor=your-docker-image)
+  docker stop $(docker ps -q --filter ancestor=one-day-a-pill-be)
+  docker rm $(docker ps -q --filter ancestor=one-day-a-pill-be)
 fi
