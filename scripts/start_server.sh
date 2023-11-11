dd8fef6205df984220dbd2fb40a56b29e23657b6
boldfaced7 <boldfaced753@gmail.com>
2023-11-11 23:59:58 +0900

    #32

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker-compose -p one-day-a-pill -f /home/ec2-user/app/docker-compose.yaml up
+sudo docker-compose -p one-day-a-pill -f /home/ec2-user/app/docker-compose.yaml up -d
